f777ddddfad8093bee5cbdf7682c9d60f0e8fc69
Niklas Carstensen <kobidogao@yahoo.com>
2024-04-23 00:27:58 +0200

    Add project files.

---
+++ see-clearly-lighthouse-prototype/Secret.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace see_clearly_lighthouse
+{
+    public static class Secret
+    {
+        public static string Username = "your username";
+        public static string Token = "your token";
+    }
+}
