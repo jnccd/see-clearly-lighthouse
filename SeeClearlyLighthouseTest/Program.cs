4fe974343b37cff9be231670e428edc57a39f127
Dobiko <kobidogao@yahoo.com>
2025-03-30 01:42:10 +0100

    Improve test

--- SeeClearlyLighthouseTest/Program.cs
+++ SeeClearlyLighthouseTest/Program.cs
@@ -5,7 +5,11 @@ var client = await new LighthouseClient(Secret.Username, Secret.Token).Connect()
 
 byte[,,] image = new byte[14, 28, 3];
 for (int i = 0; i < 14; i++)
+    image[i, 2, 0] = 128;
+for (int i = 0; i < 14; i++)
     image[i, 4, 1] = 128;
+for (int i = 0; i < 14; i++)
+    image[i, 6, 2] = 128;
 
 var res = await client.SendImage(image);
 Console.WriteLine(res);
